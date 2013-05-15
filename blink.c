da09631acc8336bed5810a7ac87dc51ee49a4a02
Simon Westphahl <westphahl@gmail.com>
2013-05-15 16:19:32 +0200

    Cleanup UART init

--- blink.c
+++ blink.c
@@ -14,28 +14,27 @@ int main (void)
     UBRR0L = UBRRL_VALUE;
 
 #if USE_2X
-    UCSR0A |= _BV(U2X0);
+    UCSR0A |= (1<<U2X0);
 #else
-    UCSR0A &= ~(_BV(U2X0));
+    UCSR0A &= ~(1<<U2X0);
 #endif
 
     /* Enable receiver and transmitter */
     UCSR0B = (1<<RXEN0)|(1<<TXEN0);
-    // UCSR0B = _BV(RXEN0) | _BV(TXEN0);
 
     /* Set frame format: 8data 2stop bit */
     UCSR0C = (1<<UCSZ01)|(1<<UCSZ00);
 
     /* set pin 5 of PORTB for output*/
-    DDRB |= _BV(DDB5);
+    DDRB |= (1<<DDB5);
  
     while(1) {
         /* set pin 5 high to turn led on */
-        PORTB |= _BV(PORTB5);
+        PORTB |= (1<<PORTB5);
         _delay_ms(BLINK_DELAY_MS);
 
         /* set pin 5 low to turn led off */
-        PORTB &= ~_BV(PORTB5);
+        PORTB &= ~(1<<PORTB5);
         _delay_ms(BLINK_DELAY_MS);
     }
 
